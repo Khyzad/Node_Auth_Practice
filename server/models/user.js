b88235a09b893df196fe5a758ebe801a14270b97
lljruffin <lljruffin@gmail.com>
2018-09-25 22:51:30 -1000

    Implement post:/user/register

--- server/models/user.js
+++ server/models/user.js
@@ -22,6 +22,7 @@ var User = module.exports = mongoose.model('User', UserSchema);
 module.exports.createUser = function (newUser, callback) {
     bcrypt.genSalt(10, function (err, salt) {
         bcrypt.hash(newUser.password, salt, function (err, hash) {
+            console.log('hash final');
             newUser.password = hash;
             newUser.save(callback);
         });
--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -19,41 +19,93 @@ router.post('/register', function (req, res) {
 
 
     var errors = req.validationErrors();
-    if (errors) {        
-        res.status(400).json({errors: errors});
+    if (errors) {
+        res.status(400).json({ errors: errors });
     } else {
-        // then check to see if the email and username are already taken
+        // check if email is already taken
         User.findOne({
-            username: {
-                "$regex": "^" + username + "\\b", "$options": "i"
-            }
-        }, function (err, user) {
+            email: email
+        }, function (err1, user1) {
             User.findOne({
-                email: {
-                    "$regex": "^" + email + "\\b", "$options": "i"
-                }
-            }, function (err, mail) {
-                if (user || mail) {
-                    res.render('register', {
-                        user: user,
-                        mail: mail
+                username: username
+            }, function (err2, user2) {
+                let err = {errors:[]};                
+                if (user1) {
+                    err.error.push({
+                        param: "email",
+                        msg: "Email is already in use"
                     });
                 }
-                else {
-                    var newUser = new User({
+                if (user2) {
+                    err.error.push({
+                        param: "username",
+                        msg: "Username is already in use"
+                    });                    
+                }
+                
+                if (err.error.length > 0) {
+                    res.status(406).json(err);
+                } else {                    
+                    User.createUser(new User({
                         name: name,
                         email: email,
                         username: username,
                         password: password
-                    });
-                    User.createUser(newUser, function (err, user) {
-                        if (err) 
-                            res.status(406).json({error: err});                        
-                    });
-                    res.status(200).json('You are registered and can now login');                    
+                    }, function (err, callback) {                        
+                        if(err){
+                            res.status(406).json({ errors: err });
+                        }else{
+                            res.status(200);
+                        }                        
+                    }));
                 }
-            });
+            });        
         });
+
+        // check if username is already taken
+        // User.findOne({
+        //     username: username
+        // }, function (err, user) {
+        //     if (user) {
+        //         res.status(406).json({ error: "Email is already in use" });
+        //     }
+        // })
+
+        // console.log('h3');
+        //res.status(200);//.json({ error: "Email is already in use" });
+        //console.log(user);
+
+        // User.findOne({
+        //     username: {
+        //         "$regex": "^" + username + "\\b", "$options": "i"
+        //     }
+        // }, function (err, user) {
+        //     User.findOne({
+        //         email: {
+        //             "$regex": "^" + email + "\\b", "$options": "i"
+        //         }
+        //     }, function (err, mail) {
+        //         if (user || mail) {
+        //             res.render('register', {
+        //                 user: user,
+        //                 mail: mail
+        //             });
+        //         }
+        //         else {
+        //             var newUser = new User({
+        //                 name: name,
+        //                 email: email,
+        //                 username: username,
+        //                 password: password
+        //             });
+        //             User.createUser(newUser, function (err, user) {
+        //                 if (err) 
+        //                     res.status(406).json({error: err});                        
+        //             });
+        //             res.status(200).json('You are registered and can now login');                    
+        //         }
+        //     });
+        // });        
     }
 });
 
